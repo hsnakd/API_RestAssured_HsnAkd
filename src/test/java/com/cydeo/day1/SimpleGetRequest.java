ed5263a325eb31fd7f42a244d6846a932da7d2aa
hsnakd <hsnakd10@gmail.com>
2022-11-08 00:20:23 +0300

    Day 2

--- src/test/java/com/cydeo/day1/SimpleGetRequest.java
+++ src/test/java/com/cydeo/day1/SimpleGetRequest.java
@@ -1,13 +1,15 @@
-package com.cydeo.Day1;
+package com.cydeo.day1;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class SimpleGetRequest {
-
+    /** ipAddress */
     String ipAddress = "3.86.235.137";
-    String url = "http://" + ipAddress + ":8000/api/spartans";
+    /** endPoint */
+    String endPoint = "/api/spartans";
+    String url = "http://" + ipAddress + ":8000" + endPoint;
 
     @Test
     public void test1(){
